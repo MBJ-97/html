d9abafe53b5d1450f19de7d67a2ce5abc631cd1b
MBJ-97 <mehdiibeldjoudi@gmail.com>
2023-03-18 15:53:15 +0100

    Added javascript code

---
+++ script.js
@@ -0,0 +1,15 @@
+//Console.log is a method that let's us print what we want on the browser's console
+// console.log('hello I am the external link!')
+
+//Let's u create alerts on browser
+// alert('hello I am the external link!')
+
+//Prompt let's you grab infos from user and use them ! 
+ function calcA() {
+    var age = prompt('What is your age ?');
+    var yourYearOfBirth = 2023 - age;
+
+    console.log(yourYearOfBirth);
+ }
+
+//  calcA()
